72ace2896c914d0ab455cbd35bba483cc6e82af0
Dylan Pinn <dylan@dylanpinn.com>
2017-08-13 14:23:40 +1000

    Add loaders to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,31 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.jpeg$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000
+            }
+          }
+        ]
+      }
+    ]
   }
 };
